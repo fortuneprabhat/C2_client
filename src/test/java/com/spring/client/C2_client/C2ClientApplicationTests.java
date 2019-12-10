fefd44f4a963453dd615f655a0938ca072cb3170
Prabhat Kumar <prabhat.kumar05@nagarro.com>
2019-12-10 16:55:08 +0530

    Discovery Client

---
+++ src/test/java/com/spring/client/C2_client/C2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.client.C2_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
